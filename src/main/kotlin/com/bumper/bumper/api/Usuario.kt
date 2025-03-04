1e04bc9d7ea6909dd197cfd34b0957a50485214f
diegoeduv <diegoeduv@ciencias.unam.mx>
2025-03-03 23:35:11 -0600

    Actualizacion para 1ra iteracion

--- src/main/kotlin/com/bumper/bumper/api/Usuario.kt
+++ src/main/kotlin/com/bumper/bumper/api/Usuario.kt
@@ -1,7 +1,21 @@
 package com.bumper.bumper.api
 
 data class Usuario(
+    @field:NotNull
+    @field:Email
     val mail: String,
+
+    @field:NotNull
+    @field:Size(min = 2, max = 50)
+    val nombre: String,
+
+    @field:NotNull
+    @field:Size(min = 2, max = 50)
+    val apellido: String,
+
+    @field:NotNull
+    @field:Size(min = 6, max = 20)
     val password: String,
-    val token: String? = null
+
+    var token: String = "inactivo"
 )
